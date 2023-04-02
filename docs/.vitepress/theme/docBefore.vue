aef9af27a40b5044dd9faee6be6094f844a7e0af
rain <1147288122@qq.com>
2023-04-02 21:59:17 +0800

    edit

---
+++ docs/.vitepress/theme/docBefore.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { useData } from 'vitepress'
+//这里的frontmatter就是各个md文件中自己写在最上面的东西
+const { frontmatter } = useData()
+</script>
+
+<template>
+  <span class="page-info">✍️{{ frontmatter.author }}</span>
+  <span class="page-info">🕐{{ frontmatter.date }}</span>
+  <span>
+    🔗
+    <span class="page-info" v-for="item in frontmatter.tags" :key="item">{{ item }}</span>
+  </span>
+</template>
+
+<style>
+  .page-info{
+    font-size: 13px;
+    color: #7f7f7f;
+    margin-right: 10px;
+  }
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,17 +3,20 @@ import { h } from 'vue'
 import { useData } from 'vitepress'
 import Theme from 'vitepress/theme'
 import './style.css'
+import xxx from './docBefore.vue'
 
 export default {
   ...Theme,
   Layout: () => {
-    const props: Record<string, any> = {}
+    const props = {}
     const { frontmatter } = useData()
     if (frontmatter.value?.layoutClass) {
       props.class = frontmatter.value.layoutClass
     }
     return h(Theme.Layout, props, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      // 使用插槽
+      // 'doc-before': () => h((xxx))
     })
   },
   enhanceApp({ app, router, siteData }) {
