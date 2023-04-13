f63a6c0c32d93f553535d6506ab253984235e93d
rain <1147288122@qq.com>
2023-04-13 22:26:05 +0800

    edit

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -1,5 +1,16 @@
 export default [
-  { text: '首页', link: '/' },
-  { text: '文档', link: '/document/configAndPlugin' },
-  { text: '生活', link: '/life/exercise' }
-]
+  {
+    text: "首页",
+    link: "/",
+  },
+  {
+    text: "文档",
+    link: "/document/configAndPlugin",
+    activeMatch: "document",
+  },
+  {
+    text: "生活",
+    link: "/life/exercise",
+    activeMatch: "/life",
+  },
+];
